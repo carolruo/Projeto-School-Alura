5db45610179453d32a2938bfa11539cc2fdad3ce
Carolina <carolinaruo@gmail.com>
2022-09-22 10:55:19 -0300

    Corrige testes automatizados de User Controller

--- src/main/java/br/com/alura/school/user/NewUserRequest.java
+++ src/main/java/br/com/alura/school/user/NewUserRequest.java
@@ -1,6 +1,7 @@
 package br.com.alura.school.user;
 
 import br.com.alura.school.support.validation.Unique;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.Email;
@@ -13,22 +14,27 @@ class NewUserRequest {
     @Unique(entity = User.class, field = "username")
     @Size(max=20)
     @NotBlank
-    @JsonProperty
+    @JsonProperty("username")
     private final String username;
 
     @Unique(entity = User.class, field = "email")
     @NotBlank
     @Email
-    @JsonProperty
+    @JsonProperty("email")
     private final String email;
 
-    NewUserRequest(String username, String email) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    NewUserRequest(@JsonProperty("username") String username, @JsonProperty("email") String email) {
         this.username = username;
         this.email = email;
     }
 
     String getUsername() {
         return username;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     User toEntity() {
