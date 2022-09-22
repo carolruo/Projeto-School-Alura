57fd28fef819bd2fa82466c2ccf1bf75bf125081
Carolina <carolinaruo@gmail.com>
2022-09-21 23:16:34 -0300

    Corrige testes automatizados de Course Controller

--- src/main/java/br/com/alura/school/course/NewCourseRequest.java
+++ src/main/java/br/com/alura/school/course/NewCourseRequest.java
@@ -1,6 +1,7 @@
 package br.com.alura.school.course;
 
 import br.com.alura.school.support.validation.Unique;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.NotBlank;
@@ -11,19 +12,17 @@ class NewCourseRequest {
     @Unique(entity = Course.class, field = "code")
     @Size(max=10)
     @NotBlank
-    @JsonProperty
     private final String code;
 
     @Unique(entity = Course.class, field = "name")
     @Size(max=20)
     @NotBlank
-    @JsonProperty
     private final String name;
 
-    @JsonProperty
     private final String description;
 
-    public NewCourseRequest(String code, String name, String description) {
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public NewCourseRequest(@JsonProperty("code") String code, @JsonProperty("name") String name, @JsonProperty("description") String description) {
         this.code = code;
         this.name = name;
         this.description = description;
@@ -31,6 +30,14 @@ class NewCourseRequest {
 
     public String getCode() {
         return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     Course toEntity() {
