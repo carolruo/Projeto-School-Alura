9c40cd943c191c9e61875b42408825f8271365c3
Carolina <carolinaruo@gmail.com>
2022-09-27 17:09:20 -0300

    Refatoração dos testes unitários, todos passam

--- src/test/java/br/com/alura/school/enroll/EnrollControllerTest.java
+++ src/test/java/br/com/alura/school/enroll/EnrollControllerTest.java
@@ -4,8 +4,8 @@ import br.com.alura.school.course.Course;
 import br.com.alura.school.course.CourseRepository;
 import br.com.alura.school.user.User;
 import br.com.alura.school.user.UserRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,11 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -30,25 +26,36 @@ public class EnrollControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    EnrollService enrollService;
-
-    @Autowired
     CourseRepository courseRepository;
     @Autowired
     UserRepository userRepository;
 
-    @Test
-    void should_add_new_enroll() throws Exception {
-        Course course = new Course("java-1", "Java OO", "Curso de Java");
-        courseRepository.save(course);
-        Set<Enroll> enrolls = new HashSet<>();
-        userRepository.save(new User("alex", "alex@gmail.com"));
-        NewEnrollRequest enrollRequest = new NewEnrollRequest("alex");
+    @AfterEach
+    void wipe() {
+        courseRepository.deleteAll();
+        userRepository.deleteAll();
+    }
 
+    @Test
+    void not_found_when_user_does_not_exist() throws Exception {
+        courseRepository.save(new Course("java-3", "Java Exceptions", "Curso de Java"));
+        NewEnrollRequest enrollRequest = new NewEnrollRequest("alexa");
 
         mockMvc.perform(post("/courses/java-1/enroll")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonMapper.writeValueAsString(enrollRequest)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonMapper.writeValueAsString(enrollRequest)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void should_add_new_enroll() throws Exception {
+        courseRepository.save(new Course("java-4", "Java Spring", "Curso de Java"));
+        userRepository.save(new User("alexa", "alexa@gmail.com"));
+        NewEnrollRequest enrollRequest = new NewEnrollRequest("alexa");
+
+        mockMvc.perform(post("/courses/java-4/enroll")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonMapper.writeValueAsString(enrollRequest)))
                 .andExpect(status().isCreated());
     }
 
--- src/test/java/br/com/alura/school/section/SectionControllerTest.java
+++ src/test/java/br/com/alura/school/section/SectionControllerTest.java
@@ -8,7 +8,7 @@ import br.com.alura.school.user.UserRepository;
 import br.com.alura.school.video.Video;
 import br.com.alura.school.video.VideoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -42,15 +42,26 @@ public class SectionControllerTest {
     @Autowired
     private VideoService videoService;
 
+    @AfterEach
+    void wipe() {
+        courseRepository.deleteAll();
+    }
+
+    @Test
+    void should_send_no_content_exception_when_no_enrollments() throws Exception {
+        mockMvc.perform(get("/sectionByVideosReport")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
 
     @Test
     void should_retrieve_section_by_videos_report() throws Exception {
-        User user = new User("alex", "alex@gmail.com");
+        User user = new User("carol", "carol@gmail.com");
         Video video1 = new Video("youtube.com/gbth45");
         Video video2 = new Video("youtube.com/je8eueu2q");
         Video video3 = new Video("youtube.com/k43kl43");
 
-        Course course1 = new Course("spring-1", "Spring Basics", "Spring Core and Spring MVC.");
+        Course course1 = new Course("spring-3", "Spring Basico", "Spring Core and Spring MVC.");
         Section section1 = new Section("spring-boot", "Initialize", "Alexa");
         section1.setCourse(course1);
         section1.setVideos(Arrays.asList(video1, video2));
@@ -59,11 +70,11 @@ public class SectionControllerTest {
         userRepository.save(user);
         Enroll enroll1 = new Enroll(user, course1);
         course1.addEnroll(enroll1);
-        enrollService.save("alex", "spring-1");
-        videoService.save(video1, "spring-1", "spring-boot");
-        videoService.save(video2, "spring-1", "spring-boot");
+        enrollService.save("carol", "spring-3");
+        videoService.save(video1, "spring-3", "spring-boot");
+        videoService.save(video2, "spring-3", "spring-boot");
 
-        Course course2 = new Course("spring-2", "Spring Boot", "Spring Boot");
+        Course course2 = new Course("spring-4", "Spring B", "Spring Boot");
         Section section2 = new Section("spring-security", "JWT Basic", "Ale");
         section2.setCourse(course2);
         section2.setVideos(Arrays.asList(video1));
@@ -71,28 +82,22 @@ public class SectionControllerTest {
         sectionRepository.save(section2);
         Enroll enroll2 = new Enroll(user, course1);
         course2.addEnroll(enroll2);
-        enrollService.save("alex", "spring-2");
-        videoService.save(video3, "spring-2", "spring-security");
+        enrollService.save("carol", "spring-4");
+        videoService.save(video3, "spring-4", "spring-security");
 
         mockMvc.perform(get("/sectionByVideosReport")
                         .accept(MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk())
                         .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                         .andExpect(jsonPath("$.length()", is(2)))
-                        .andExpect(jsonPath("$[0].courseName", is("Spring Basics")))
+                        .andExpect(jsonPath("$[0].courseName", is("Spring Basico")))
                         .andExpect(jsonPath("$[0].sectionTitle", is("Initialize")))
                         .andExpect(jsonPath("$[0].authorName", is("Alexa")))
                         .andExpect(jsonPath("$[0].totalVideos", is(2)))
-                        .andExpect(jsonPath("$[1].courseName", is("Spring Boot")))
+                        .andExpect(jsonPath("$[1].courseName", is("Spring B")))
                         .andExpect(jsonPath("$[1].sectionTitle", is("JWT Basic")))
                         .andExpect(jsonPath("$[1].authorName", is("Ale")))
                         .andExpect(jsonPath("$[1].totalVideos", is(1)));
     }
 
-    @Test
-    void should_send_no_content_exception_when_no_enrollments() throws Exception {
-        mockMvc.perform(get("/sectionByVideosReport")
-                        .accept(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isNoContent());
-    }
 }
