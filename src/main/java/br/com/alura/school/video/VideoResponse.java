4a9300ffd172243f4549420750121f502d1bb93d
Carolina <carolinaruo@gmail.com>
2022-09-27 20:15:09 -0300

    Cria VideoResponse

---
+++ src/main/java/br/com/alura/school/video/VideoResponse.java
@@ -0,0 +1,24 @@
+package br.com.alura.school.video;
+
+import br.com.alura.school.section.Section;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class VideoResponse {
+
+    @JsonProperty
+    private final String video;
+
+    @JsonProperty
+    private final Section section;
+
+    public VideoResponse(String video, Section section) {
+        this.video = video;
+        this.section = section;
+    }
+
+    private String abbreviateDescription(String description) {
+        if (description.length() <= 13) return description;
+        return description.substring(0, 10) + "...";
+    }
+
+}
